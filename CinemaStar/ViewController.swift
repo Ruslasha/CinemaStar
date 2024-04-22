a0fb9a4204d739d56ec0f4f6292934e30acd4abd
Ruslan <ruslan99992222@gmail.com>
2024-04-22 19:58:40 +0300

    Initial Commit

---
+++ CinemaStar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinemaStar
+//
+//  Created by Руслан Абрамов on 22.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
